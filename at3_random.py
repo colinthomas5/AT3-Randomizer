e70083c7b8fdd2af3d12375d6eeeadd389037222
colin <74887052+colinthomas5@users.noreply.github.com>
2022-07-29 20:42:02 -0400

    Add files via upload
    0.2.0

--- at3_random.py
+++ at3_random.py
@@ -1,17 +1,18 @@
 import promptlib
 import random
+import os
 
 prompter = promptlib.Files()
 
 # List of files to be randomized within data folder
-fileList = ["\\castle_basement_master.pak", "\\castle_nightmare_master.pak", "\\global.pak", "\\overworld_forest_cave_2.pak", "\\overworld_forest_master.pak", "\\overworld_iceking_cave.pak",
+fileList = ["\\castle_basement_master.pak", "\\castle_nightmare_master.pak", "\\overworld_forest_cave_2.pak", "\\overworld_forest_master.pak", "\\overworld_iceking_cave.pak",
 			"\\overworld_kingdom_master.pak", "\\overworld_mountain_cave_3.pak", "\\overworld_mountain_cave_4.pak", "\\overworld_mountain_master.pak", "\\overworld_swamp_master.pak",
 			"\\overworld_wasteland_cave_1.pak", "\\temple_dream_master.pak", "\\temple_fear_master.pak", "\\temple_song_master.pak"]
 
 # Files that could be in fileList but are currently excluded
-#fileList = ["\\overworld_lsp_cave.pak"]
+#fileList = ["\\overworld_lsp_cave.pak", "\\global.pak"]
 
-# Used for debugging, lists most files within data folder to scout for files that can be added to pooling and randomizing
+# Used for debugging, lists most files within data folder to scout for files that can be added to pooling and randomizing but aren't due to lack of items found by program
 #fileList = ["\\arena_hairapes.pak", "\\autoload.pak", "\\boss_crabdemon.pak", "\\boss_hairapes.pak", "\\boss_nightmare.pak", "\\boss_shadowfinn.pak", "\\castle_basement_master.pak",
 #			"\\castle_nightmare_master.pak", "\\caves.pak", "\\dreamtemple.pak", "\\fearcastle.pak", "\\feartemple.pak", "\\global.pak", "\\gym_crabdemon.pak", "\\gym_nprincess.pak",
 #			"\\gym_quests.pak", "\\gym_shadowfinn.pak", "\\npc_choose_goose.pak", "\\npc_coalman.pak", "\\npc_iceking.pak", "\\npc_magicman.pak", "\\npc_marceline.pak", "\\npc_mr_pig.pak",
@@ -20,29 +21,6 @@ fileList = ["\\castle_basement_master.pak", "\\castle_nightmare_master.pak", "\\
 #			"\\overworld_kingdom_master.pak", "\\overworld_lsp_cave.pak", "\\overworld_mountain.pak", "\\overworld_mountain_cave_1.pak", "\\overworld_mountain_cave_2.pak", "\\overworld_mountain_cave_3.pak",
 #			"\\overworld_mountain_cave_4.pak", "\\overworld_mountain_master.pak", "\\overworld_swamp.pak", "\\overworld_swamp_master.pak", "\\overworld_wasteland_cave_1.pak", "\\songtemple.pak",
 #			"\\temple_dream_master.pak", "\\temple_fear_master.pak", "\\temple_song_master.pak", "\\temples.pak"]
-
-
-# List of items in item pool [DEPRICATED, NOW BUILDS ITEM POOL WITH itemLocal]
-#itemPool = ["PickupChestItemKey", "PickupTreasureHuge", "PickupHealthOne", "PickupHealthOne", "PickupTrailMix", "PickupTreasureHuge", "PickupTreasureHuge", "PickupTreasureHuge", "PickupSpareThumps", 
-#			"PickupTreasureHuge", "PickupTrailMix3", "PickupChestItemKey", "PickupChestItemKey", "PickupChestItemKey", "PickupChestItemKey", "PickupChestItemKey", "PickupMeat", "PickupHeartPiece", 
-#			"PickupPencil", "PickupSweater", "PickupMapPaper", "PickupChestItemKey", "PickupChestItemKey", "PickupChestItemKey", "PickupChestItemKey", "PickupPieFairy", "PickupChestItemRBK", "PickupPieFairy", 
-#			"PickupHealthOne", "PickupTreasureSmall", "PickupTreasureSmall", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupTreasureSmall", "PickupHealthOne", 
-#			"PickupHealthOne", "PickupHealthOne", "PickupTreasureBig", "PickupDemonHeart", "PickupNuts", "PickupFruits", "PickupHealthOne", "PickupTreasureBig", "PickupHealthOne", "PickupHealthOne", 
-#			"PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", 
-#			"PickupHealthOne", "PickupHealthOne", "PickupFruits", "PickupNuts", "PickupDemonHeart", "PickupTreasureBig", "PickupNuts", "PickupFruits", "PickupHealthOne", "PickupNuts", "PickupTreasureBig", 
-#			"PickupFruits", "PickupGumGlobe", "PickupGumGlobe", "PickupGumGlobe", "PickupGumGlobe", "PickupGumGlobe", "PickupWoodPlank", "PickupHeartPiece", "PickupHeartPiece", "PickupGumGlobe", "PickupGumGlobe", 
-#			"PickupGumGlobe", "PickupHeartPiece", "PickupPlasticBag", "PickupWoodPlank", "PickupGumGlobe", "PickupGumGlobe", "PickupPieFairy", "PickupGumGlobe", "PickupGumGlobe", "PickupGumGlobe", "PickupGumGlobe", 
-#			"PickupGumGlobe", "PickupGumGlobe", "PickupWoodPlank", "PickupHeartPiece", "PickupGumGlobe", "PickupPieFairy", "PickupWoodPlank", "PickupGumGlobe", "PickupGumGlobe", "PickupGumGlobe", "PickupTreasureHuge", 
-#			"PickupGumGlobe", "PickupWoodPlank","PickupGumGlobe ", "PickupPencil", "PickupMapPaper", "PickupTreasureHuge", "PickupChestItemKey", "PickupChestItemKey", "PickupSpareThumps", "PickupTreasureHuge", "PickupFruitStack", 
-#			"PickupTreaureHuge", "PickupHeroGauntlet", "PickupChestItemKey", "PickupChestItemKey", "PickupChestItemKey", "PickupChestItemRBK", "PickupChestItemKey", "PickupGumGlobe", "PickupPieFairy", 
-#			"PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", 
-#			"PickupHealthOne", "PickupTrailMix1", "PickupHealthOne", "PickupHealthOne", "PickupChestItemKey", "PickupTreasureHuge", "PickupChestItemKey", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", 
-#			"PickupHealthOne", "PickupHealthOne", "PickupDemonHeart", "PickupNuts", "PickupFruits", "PickupTreasureBig", "PickupMapPaper", "PickupChestItemRBK", "PickupPencil", "PickupChestItemKey", "PickupGumGlobe", 
-#			"PickupSpareThumps", "PickupFruitStack", "PickupPieFairy", "PickupTrailMix2", "PickupHealthOne", "PickupPieFairy", "PickupFruitStack", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", 
-#			"PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", 
-#			"PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupHealthOne", "PickupTrailMix3", "PickupChestItemKey", "PickupChestItemKey", "PickupMapPaper", "PickupChestItemRBK", "PickupChestItemKey", 
-#			"PickupPencil", "PickupGrabbyHand", "PickupGumGlobe", "PickupTreasureHuge", "PickupTreasureHuge", "PickupChestItemKey", "PickupTreasureSmall", "PickupPieFairy"]
-
 
 # List of items to check randomization; Null char added to each entry to maintain a consistent length of 19
 itemList = ["PickupChestItemKey\0", "PickupTreasureHuge\0", "PickupTrailMix\0\0\0\0\0", "PickupSpareThumps\0\0", "PickupTrailMix3\0\0\0\0", "PickupMeat\0\0\0\0\0\0\0\0\0", "PickupHeartPiece\0\0\0", 
@@ -56,79 +34,121 @@ itemList = ["PickupChestItemKey\0", "PickupTreasureHuge\0", "PickupTrailMix\0\0\
 # List of local item pool
 itemLocal = []
 
-# Choose data folder to randomize
+# Choose data folder to randomize, initializes seed and spoiler log
 print("Choose the AT3 data folder that you wish to randomize.")
 dir = prompter.dir()
+seed = input("Type the seed you wish to use (Leave blank to enter a random seed): ")
+customSeed = 2
+if seed == "":
+	seed = random.random()
+	random.seed(seed)
+	customSeed = 0
+else:
+	random.seed(seed, version=1)
+	customSeed = 1
+spoiler = 2
+while spoiler is 2:
+	createLog = input("Do you want a spoiler log to be generated? (Y/N): ")
+	if createLog == "Y" or createLog == "y":
+		logPath = os.getcwd() + "\\spoiler.log"
+		log = open(logPath, 'w')
+		logSeed = "seed: " + str(seed) + "\n \n"
+		spoilerLog = []
+		spoilerLog.append(logSeed)
+		spoiler = 1
+	if createLog == "N" or createLog == "n":
+		spoiler = 0
+
+
 
 # Builds local item pool while replacing items with placeholder text to be changed into randomized items; placeholder text named to have length of 19
 for area in fileList:
 	path = dir + area
-	currently = "Now generating pool from: " + path
-	print(currently)
+	print("Now generating pool from: ", path)
 	openFile = open(path, "r+", encoding="latin-1", newline='')
 	line = openFile.readlines()
+	areaClean = area.lstrip("\\").rstrip(".pak") + ": \n"
 	c = 0
 	lines = []
-	for index in line:
+	if spoiler is 1:
+		spoilerLog.append(areaClean)
+	for location in line:
 		if area == "\\castle_nightmare_master.pak":
-			while "PickupSweater\0\0\0\0\0\0" in index:
+			while "PickupSweater\0\0\0\0\0\0" in location:
 				itemLocal.append("PickupSweater\0\0\0\0\0\0")
 				replacement = line[c].replace("PickupSweater\0\0\0\0\0\0", "placeholdertextomg!", 1)
-				replaced = "Replaced PickupSweater\0\0\0\0\0\0 with placeholder"
 				line[c] = replacement.lstrip('')
-				index = line[c]
-				print(replaced)
+				location = line[c]
+				print("Replaced PickupSweater\0\0\0\0\0\0 with placeholder")
+				if spoiler is 1:
+					spoilerLog.append("PickupSweater\0\0\0\0\0\0 -> ")
 		else:
 			for item in itemList:
-				while item in index:
+				while item in location:
 					itemLocal.append(item.ljust(19))
 					replacement = line[c].replace(item, "placeholdertextomg!", 1)
-					replaced = "Replaced " + item + " with placeholder"
 					line[c] = replacement.lstrip('')
-					index = line[c]
-					print(replaced)
+					location = line[c]
+					print("Added ", item, " to item pool")
+					if spoiler is 1:
+						i = item + " ->  "
+						spoilerLog.append(i)
 		lines.append(line[c])
 		c += 1
+	if spoiler is 1:
+		spoilerLog.append( "\n")
 	openFile.seek(0)
 	openFile.truncate(0)
 	for line in lines:
 		openFile.write(line)
 	openFile.close()
-	success = "Successfully pooled from " + path
-	print(success)
+	print("Successfully pooled from ", path)
 
 # Replaces placeholders with actual items from local item pool
 for area in fileList:
 	path = dir + area
-	currently = "Now randomizing: " + path
-	print(currently)
+	print("Now randomizing ", path)
 	openFile = open(path, "r+", encoding="latin-1", newline='')
 	line = openFile.readlines()
+	areaClean = area.lstrip("\\").rstrip(".pak") + ": \n"
 	c = 0
+	s = 0
 	lines = []
-	for index in line:
+	for location in line:
 		if area == "\\castle_nightmare_master.pak":
-			while "placeholdertextomg!" in index:
+			while "placeholdertextomg!" in location:
 				length = len(itemLocal)
 				randomNumber = random.randint(0, length-1)
 				while itemLocal[randomNumber] == "PickupGrabbyHand\0\0\0" or itemLocal[randomNumber] == "PickupHeroGauntlet\0":
 					randomNumber = random.randint(0, length-1)
 				replacement = line[c].replace("placeholdertextomg!", itemLocal[randomNumber], 1)
-				replaced = "Replaced placeholder with " + itemLocal[randomNumber]
 				line[c] = replacement.lstrip(' ')
-				index = line[c]
+				location = line[c]
+				print("Replaced placeholder with ", itemLocal[randomNumber])
+				if spoiler is 1:
+					for entry in spoilerLog:
+						if entry == areaClean:
+							s += 1
+							logIndex = int(spoilerLog.index(entry) + s)
+							logEntry = spoilerLog[logIndex] + itemLocal[randomNumber] + "\n"
+							spoilerLog[logIndex] = logEntry
 				itemLocal.remove(itemLocal[randomNumber])
-				print(replaced)
 		else:
-			while "placeholdertextomg!" in index:
+			while "placeholdertextomg!" in location:
 				length = len(itemLocal)
 				randomNumber = random.randint(0, length-1)
 				replacement = line[c].replace("placeholdertextomg!", itemLocal[randomNumber], 1)
-				replaced = "Replaced placeholder with " + itemLocal[randomNumber]
 				line[c] = replacement.lstrip(' ')
-				index = line[c]
+				location = line[c]
+#				print("Replaced placeholder with ", itemLocal[randomNumber])
+				if spoiler is 1:
+					for entry in spoilerLog:
+						if entry == areaClean:
+							s += 1
+							logIndex = int(spoilerLog.index(entry) + s)
+							logEntry = spoilerLog[logIndex] + itemLocal[randomNumber] + "\n"
+							spoilerLog[logIndex] = logEntry
 				itemLocal.remove(itemLocal[randomNumber])
-				print(replaced)
 			lines.append(line[c])
 		c += 1
 	openFile.seek(0)
@@ -136,7 +156,15 @@ for area in fileList:
 	for line in lines:
 		openFile.write(line)
 	openFile.close()
-	success = "Successfully randomized " + path
-	print(success)
-input("Randomization complete! Press any key to close...")
+	print("Successfully randomized ", path)
+if customSeed is 0:
+	print("Your seed is: ", seed)
+if spoiler is 1:
+	logPath = os.getcwd() + "\\spoiler.log"
+	log = open(logPath, 'w')
+	for entry in spoilerLog:
+		log.write(str(entry))
+	print("Log saved to ", os.getcwd(), "\\spoiler.log")
+	log.close()
+input("Randomization complete! Press enter or exit the window to close.")
 
